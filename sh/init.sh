bf0b4e35b0816d9074a2eaffc5ce30686c0b2572
尤雨东 <root@ltpp.vip>
2025-02-07 22:54:07 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add jihulab git@jihulab.com:ltpp-universe/async-func.git;
+git remote add gitee git@gitee.com:ltpp-universe/async-func.git;
 git remote add github git@github.com:ltpp-universe/async-func.git;
 git remote add origin git@github.com:ltpp-universe/async-func.git;
