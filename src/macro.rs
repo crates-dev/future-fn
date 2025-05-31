67f40d35f9e1d4232e0fbeea1d890504c50056a4
尤雨东 <root@ltpp.vip>
2025-05-31 13:45:01 +0800

    feat: v0.2.6

--- src/macro.rs
+++ src/macro.rs
@@ -1,20 +1,8 @@
-/// Creates an asynchronous function that takes a closure with parameters and executes it asynchronously.
+/// Generates an asynchronous closure that clones specified external variables and executes the given closure body asynchronously.
 ///
-/// This macro generates an asynchronous closure that captures external variables by cloning them, and
-/// allows the closure to be invoked with the specified parameters. The body of the closure is executed
-/// inside an asynchronous block.
-///
-/// # Parameters
-/// - `$($var:ident),*` : A list of variables from the outer scope to be captured and cloned inside the closure.
-/// - `$($closure_param:ident),*` : A list of parameters for the closure. These parameters are used when calling the closure.
-/// - `{ $($closure_body:tt)* }` : The body of the closure that is executed asynchronously.
-///
-/// # Returns
-/// A closure that takes the specified parameters and returns a `Pin<Box<dyn Future<Output = ()> + Send>>`
-/// which is an asynchronous task that can be awaited.
-///
-/// This macro allows you to encapsulate external state (captured via cloning) and run an asynchronous
-/// task with a specified closure.
+/// This macro supports two forms:
+/// 1. An async closure with no parameters.
+/// 2. An async closure with specified parameters.
 #[macro_export]
 macro_rules! future_fn {
     ($($var:ident),*, { $($closure_body:tt)* }) => {
