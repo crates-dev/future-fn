2d1b8ea619c6a7fcb333aced2df50053474c7afe
尤雨东 <root@ltpp.vip>
2025-07-27 20:40:45 +0800

    feat: v0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+//! future-fn
+//!
+//! A Rust library providing macros to simplify the creation of
+//! asynchronous closures with external state captured by move.
+//! Useful for structuring asynchronous code with ease and clarity.
+
 pub(crate) mod cfg;
 pub(crate) mod r#macro;
+
 #[allow(unused_imports)]
 pub use r#macro::*;
